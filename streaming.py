84064295ecff2df568d16900d73a24d7c1d6b015
Academy <jakil93@naver.com>
2017-10-18 12:20:39 +0900

    kty : password function add

--- streaming.py
+++ streaming.py
@@ -8,8 +8,8 @@ def gen():
         cam = cv2.VideoCapture(0)
 
         while True:
-            ret,img = cam.read()
-            frame=cv2.imencode(".jpeg",img)[1].tostring()
+            ret, img = cam.read()
+            frame=cv2.imencode(".mp4",img)[1].tostring()
             yield (b'--frame\r\n' b'Content-Type: video/mp4\r\n\r\n' + frame + b'\r\n')
 
         del(cam)
