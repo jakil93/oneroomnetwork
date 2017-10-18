93b29b63caee47b1fedd54882ed5105b8ec1b156
Academy <jakil93@naver.com>
2017-10-18 12:18:38 +0900

    kty : password function add

--- MainController.py
+++ MainController.py
@@ -120,7 +120,7 @@ def video_feed():
 
     try:
         #result = Response(img, mimetype='multipart/x-mixed-replace; boundary=frame')
-        result = Response(img, mimetype='video/mp4')
+        result = Response(img, mimetype='video/mp4; boundary=frame')
     except:
         print("error")
         result = None
--- streaming.py
+++ streaming.py
@@ -10,7 +10,7 @@ def gen():
         while True:
             ret,img = cam.read()
             frame=cv2.imencode(".jpeg",img)[1].tostring()
-            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            yield (b'--frame\r\n' b'Content-Type: video/mp4\r\n\r\n' + frame + b'\r\n')
 
         del(cam)
     except:
