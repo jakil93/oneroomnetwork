1d77da9723ed303a8449a2bfcb0da8dff49c1689
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-10-26 17:14:07 +0900

    Merge branch 'master' of https://github.com/jakil93/oneroomnetwork
    # Conflicts:
    #	streaming.py

--- motion.py
+++ motion.py
@@ -52,7 +52,7 @@ def release(out):
 
 def getOutput(frame):
     h, w, _ = frame.shape
-    out = cv2.VideoWriter('static/video/' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + '.mp4', fourcc, 28, (w, h), True)
+    out = cv2.VideoWriter('static/video/' + str(datetime.now().strftime('%Y-%m-%d:%H%M%S')) + '.mp4', fourcc, 28, (w, h), True)
 
     return out
 
