983d85cc22c36cb04cbc092678cb897c77a42a16
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-10-27 09:51:44 +0900

    Merge branch 'master' of https://github.com/jakil93/oneroomnetwork
    # Conflicts:
    #	streaming.py

--- servo.py
+++ servo.py
@@ -1,21 +1,25 @@
 import RPi.GPIO as GPIO
 import time
-
-pin = 14
+pin = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
-p = GPIO.PWM(pin, 50)
+p = GPIO.PWM(pin,50)
+p.start(12.5)
+time.sleep(0.5)
+p.stop()
 p.start(7.5)
-
+time.sleep(0.5)
+p.stop
+p.start(10.0)
+time.sleep(0.5)
+p.stop()
+var = 1
 try:
     while True:
-        p.ChangeDutyCycle(7.5)
+        var = input("input : ")
+        p.start(var)
         time.sleep(1)
-        p.ChangeDutyCycle(12.5)
-        time.sleep(1)
-        p.ChangeDutyCycle(2.5)
-        time.sleep(3)
+        p.stop()
 
 except KeyboardInterrupt:
-    p.stop()
     GPIO.cleanup()
