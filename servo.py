69fb6fd5469e5a0536e995bead1548c547a71b4c
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-10-27 12:06:14 +0900

    Merge branch 'master' of https://github.com/jakil93/oneroomnetwork
    # Conflicts:
    #	streaming.py

--- servo.py
+++ servo.py
@@ -5,23 +5,22 @@ pin = 18  # PWM pin num 18
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
-p = GPIO.PWM(pin, 50)
 cnt = 0
-
 pre = 0
 
 try:
     while True:
-        p.start(0)
-        var = input("input : ")
-        if(pre != int(var)):
-            p.ChangeDutyCycle(int(var))
-            print("angle :",int(var))
-        else:
-            print("keep")
-        pre = int(var)
-        time.sleep(1)
-        p.stop()
+        with GPIO.PWM(pin,50) as p:
+            p.start(0)
+            var = input("input : ")
+            if(pre != int(var)):
+                p.ChangeDutyCycle(int(var))
+                print("angle :",int(var))
+            else:
+                print("keep")
+            pre = int(var)
+            time.sleep(1)
+            p.stop()
 
 except KeyboardInterrupt:
     p.stop()
