0d70da13964e359e9e1c4aa864972a223ceb9ea7
DESKTOP-KKAQ0RK\it <kiru923@gmail.com>
2017-10-27 12:14:17 +0900

    Merge branch 'master' of https://github.com/jakil93/oneroomnetwork
    # Conflicts:
    #	streaming.py

--- servo.py
+++ servo.py
@@ -4,12 +4,13 @@ import time
 pin = 18  # PWM pin num 18
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)
+# GPIO.setup(pin, GPIO.OUT)
 cnt = 0
 pre = 0
 
 try:
     while True:
+        GPIO.setup(pin, GPIO.OUT)
         p = GPIO.PWM(pin,50)
         p.start(0)
         var = input("input : ")
